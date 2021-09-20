774ee1c76c3c252adee448610d7c1f17405727c6
hernandomartin12 <75068960+hernandomartin12@users.noreply.github.com>
2021-09-20 20:00:12 +0700

    Week04

---
+++ Week04_36671/Week04_36671/Week04b_36671/app/src/main/java/id/ac/umn/week04b_36671/MainActivity.java
@@ -0,0 +1,35 @@
+package id.ac.umn.week04b_36671;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public Button button_1, button_2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button_1 = findViewById(R.id.main_button_change_1);
+        button_2 = findViewById(R.id.main_button_change_2);
+
+        button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
